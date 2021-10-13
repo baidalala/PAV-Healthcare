5c5380e213584827e68d4845bba232e397ed16f2
frappe <frappe@ubuntu.vm>
2021-10-13 12:41:04 +0300

    Version 2

--- pav_healthcare/pav_healthcare/doctype/commission_compute_item/commission_compute_item.js
+++ pav_healthcare/pav_healthcare/doctype/commission_compute_item/commission_compute_item.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Partner Consulting Solutions and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Commissions', {
+frappe.ui.form.on('Commission Compute Item', {
 	// refresh: function(frm) {
 
 	// }
