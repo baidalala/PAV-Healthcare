3bcd2fe0a272643b480e538a836827c41ba84445
frappe <frappe@ubuntu.vm>
2021-10-20 13:57:30 +0300

    BB

--- pav_healthcare/pav_healthcare/doctype/commission_compute/commission_compute.py
+++ pav_healthcare/pav_healthcare/doctype/commission_compute/commission_compute.py
@@ -25,182 +25,222 @@ class CommissionCompute(AccountsController):
 			if item.sii_no:
 				doc = frappe.get_doc('Sales Invoice Item',item.sii_no)				
 				if cancel==False:
-					doc.db_set("external", 1, update_modified=False)
-					doc.db_set("internal", 1, update_modified=False)
-					doc.db_set("marketer", 1, update_modified=False)
+					if item.external_doctor:
+						doc.db_set("external", 1, update_modified=False)
+					if item.internal_doctor:
+						doc.db_set("internal", 1, update_modified=False)
+					if item.marketer:
+						doc.db_set("marketer", 1, update_modified=False)
 				else:
-					doc.db_set("external", 0, update_modified=False)
-					doc.db_set("internal", 0, update_modified=False)
-					doc.db_set("marketer", 0, update_modified=False)
-			
-			
+					if item.external_doctor:					
+						doc.db_set("external", 0, update_modified=False)
+					if item.internal_doctor:
+						doc.db_set("internal", 0, update_modified=False)
+					if item.marketer:
+						doc.db_set("marketer", 0, update_modified=False)
 
 	def on_cancel(self):
 		self.make_gl_entries(cancel=True)
 
 	def make_gl_entries(self, cancel = False):
-
-		gl_entries = self.get_gl_entries()
-
+		gl_entries=[]
+		self.make_commissions_gl_entry(gl_entries,mark_as_calc=cancel)
 		if gl_entries:
-			make_gl_entries(gl_entries,  cancel= cancel)
+			make_gl_entries(gl_entries, cancel = cancel)
 
-	def get_gl_entries(self, warehouse_account=None):
-		gl_entries = []
-		self.make_commissions_gl_entry(gl_entries)
-	
-		gl_entries = merge_similar_entries(gl_entries)
-
-		return gl_entries
-
-	def make_commissions_gl_entry(self, gl_entries):
+	def make_commissions_gl_entry(self, gl_entries,mark_as_calc=True):
 		for row in self.get("items"):
-					
-			gl_entries.append(
+			doc = frappe.get_doc('Sales Invoice Item',row.sii_no)
+			total=0.0
+			if row.total_external_doctor>0.0:
+				# doc.db_set("external", mark_as_calc, update_modified=False)
+				gl_entries.append(
 					self.get_gl_dict({
 						"posting_date":self.posting_date,
-						"account": row.external_account,
+						"account": self.external_account,
 						"party_type": "Sales Partner",
 						"party": row.external_doctor,						
-						"credit": row.external_doctor_amount,
-						"credit_in_account_currency": row.external_doctor_amount,
+						"credit": flt((row.total_external_doctor*self.exchange_rate),2),
+						"credit_in_account_currency":flt(( row.total_external_doctor),2),
+						"account_currency":row.transaction_currency,
 						"against_voucher": self.name,
 						"against_voucher_type": self.doctype,
 						"remarks": self.get("remarks") or _("Accounting Entry for External Doctor"),
 						"cost_center": row.cost_center,
 						"project": row.project 
 					}, item=self))
-			gl_entries.append(
+				total+=flt(( row.total_external_doctor),2)
+			if row.total_internal_doctor>0.0:
+				# doc.db_set("internal", mark_as_calc, update_modified=False)
+				gl_entries.append(
 					self.get_gl_dict({
 						"posting_date":self.posting_date,
-						"account": row.marketer_account,
-						"party_type": "Sales Partner",
-						"party": row.marketer,						
-						"credit": row.market_amount,
-						"credit_in_account_currency": row.market_amount,
-						"against_voucher": self.name,
-						"against_voucher_type": self.doctype,
-						"remarks": self.get("remarks") or _("Accounting Entry for Marketer"),
-						"cost_center": row.cost_center,
-						"project": row.project 
-					}, item=self))
-			
-			gl_entries.append(
-					self.get_gl_dict({
-						"posting_date":self.posting_date,
-						"account": row.internal_account,
+						"account": self.internal_account,
 						"party_type": "Healthcare Practitioner",
 						"party": row.internal_doctor,						
-						"credit": row.internal_party_amount,
-						"credit_in_account_currency": row.internal_party_amount,
+						"credit": flt((row.total_internal_doctor*self.exchange_rate),2),
+						"credit_in_account_currency": flt((row.total_internal_doctor),2),
+						"account_currency":row.transaction_currency,
 						"against_voucher": self.name,
 						"against_voucher_type": self.doctype,
 						"remarks": self.get("remarks") or _("Accounting Entry for Internal Doctor"),
 						"cost_center": row.cost_center,
 						"project": row.project 
 					}, item=self))
-
-			gl_entries.append(
-				self.get_gl_dict({
-					"posting_date":self.posting_date,
-					"account": row.item_account,
-					"debit": row.total_commission,
-					"debit_in_account_currency": row.total_commission,
-					"against_voucher": self.name,
-					"against_voucher_type": self.doctype,
-					"remarks": self.get("remarks") or _("Accounting Entry for commissions"),
-					"cost_center": row.cost_center,
-					"project": row.project 
-				}, item=self))
+				total+=flt(( row.total_internal_doctor),2)
+			if row.total_marketer:
+				# doc.db_set("marketer", mark_as_calc, update_modified=False)
+				gl_entries.append(
+					self.get_gl_dict({
+						"posting_date":self.posting_date,
+						"account": self.marketer_account,
+						"party_type": "Sales Partner",
+						"party": row.marketer,						
+						"credit": flt((row.total_marketer*self.exchange_rate),2),
+						"credit_in_account_currency": flt((row.total_marketer),2),
+						"account_currency":row.transaction_currency,
+						"against_voucher": self.name,
+						"against_voucher_type": self.doctype,
+						"remarks": self.get("remarks") or _("Accounting Entry for Marketer"),
+						"cost_center": row.cost_center,
+						"project": row.project 
+					}, item=self))	
+				total+=flt(( row.total_marketer),2)
+			if total>0.0:		
+				gl_entries.append(
+					self.get_gl_dict({
+						"posting_date":self.posting_date,
+						"account": row.item_account,
+						"debit": flt((total*self.exchange_rate),2),
+						"debit_in_account_currency": flt((total),2),
+						"against_voucher": self.name,
+						"against_voucher_type": self.doctype,
+						"remarks": self.get("remarks") or _("Accounting Entry for commissions"),
+						"cost_center": row.cost_center,
+						"project": row.project 
+					}, item=self))
+		# frappe.msgprint("gl_entries={0}".format(gl_entries))
 
 	def get_commissions_list(self):
 		
-		return frappe.db.sql("""select si.name as sale,sii.item_code,sii.item_name,sii.qty,sii.rate,si.sales_partner_name
-		,pe.practitioner,sii.name as sii_no
+		return frappe.db.sql("""select sii.external,sii.internal,sii.marketer, si.name as sale,sii.item_code,sii.item_name,sii.qty,sii.rate,si.sales_partner_name,sii.income_account,sii.cost_center,
+		(select pmr.practitioner from `tabPatient Medical Record` pmr where pmr.sales_invoice=si.name and pmr.item=sii.item_code limit 1) as practitioner,
+		(select pmr.amount from `tabPatient Medical Record` pmr where pmr.sales_invoice=si.name and pmr.item=sii.item_code limit 1) as practitioner_rate,sii.name as sii_no,sp.sales_partner_marketer
 		
 		from `tabSales Invoice Item` sii
 		INNER JOIN  `tabSales Invoice` si 
 		ON sii.parent=si.name
+		INNER JOIN  `tabSales Partner` sp
+		ON si.sales_partner_name=sp.name
 
-		JOIN `tabPatient Encounter` pe
-		ON pe.sales_invoice=si.name
+		INNER JOIN  `tabAccount` acc
+		ON sii.income_account=acc.name
+		
+		where si.currency=%s and acc.account_currency=%s and si.posting_date >= %s and si.posting_date <= %s and si.docstatus=1 and sii.external=0 and sii.internal=0 and sii.marketer=0
+		
+		 """,[self.transaction_currency,self.transaction_currency,self.from_date,self.to_date], as_dict=True)
 
-		
-		where si.posting_date between %s and %s and si.docstatus=1 and sii.external=0 and sii.internal=0 and sii.marketer=0
-		
-		 """,[self.from_date,self.to_date], as_dict=True)
-	
+	def get_exchange_rate(self):
+		return frappe.db.sql("""select exchange_rate as rate
+		from `tabCurrency Exchange`
+		where from_currency= %s and to_currency= %s order by date desc
+		 """,[self.c_currency,self.transaction_currency], as_dict=True)
+
 	def fill_commission(self):
 		self.items=None
+		self.transaction_currency=frappe.db.get_single_value("PAV Healthcare Settings", "transaction_currency")
+		exchange_rate=self.get_exchange_rate()
+		self.exchange_rate=exchange_rate[0].rate
+		# frappe.msgprint(transaction_currency)
 		commissions = self.get_commissions_list()
 		if not commissions:
 				frappe.msgprint(_("No commissions"))
 		for d in commissions:
 			row=self.append('items', {})
+			row.transaction_currency=self.transaction_currency
 			row.sales_invoice=d.sale
 			row.item_code=d.item_code
 			row.item_name=d.item_name
 			row.qty=d.qty
 			row.amount=d.rate
-			row.external_doctor=d.sales_partner_name
-			row.internal_doctor=d.practitioner
+			if not d.external:
+				row.external_doctor=d.sales_partner_name
+			if not d.internal:
+				row.internal_doctor=d.practitioner
 			details=get_item_detail(self.company,d.item_code)
 			# frappe.msgprint(frappe.as_json(details))
-			row.item_account=details['income_account']
+			row.item_account=d.income_account
 			row.item_group=details['item_group']
-			row.cost_center=details['cost_center']
-			row.project=details['project']
-			internal_amount=get_item_commission(d.item_code,row.item_group,d.practitioner)
-			if internal_amount:
-				row.internal_party_amount=internal_amount[0].rate
-			external_amount=get_item_commission(d.item_code,row.item_group,d.sales_partner_name)
-			if external_amount:
-				row.external_doctor_amount=external_amount[0].rate
-			marketer=get_marketer(d.sales_partner_name)
-			if marketer:
-				row.marketer=marketer[0].sales_partner_marketer
-				marketer_amount=get_item_commission(d.item_code,row.item_group,row.marketer)
-				if marketer_amount:
-					row.market_amount=marketer_amount[0].rate
-			row.total_commission=row.internal_party_amount+row.external_doctor_amount+row.market_amount
+			row.cost_center=d.cost_center
+			# row.project=details['project']
+			row.total_internal_doctor=0.0
+			row.external_doctor_amount=0.0
+			row.market_amount=0.0
+			if d.practitioner:
+				rate=get_item_commission(item=d.item_code,item_group=row.item_group,party=d.practitioner,party_type='Healthcare Practitioner')
+				# row.internal_party_amount=flt(n*self.exchange_rate)
+				row.total_internal_doctor=flt(rate*row.qty)
+			
+			if not d.marketer:
+				if d.sales_partner_name:
+					rate=get_item_commission(item=d.item_code,item_group=row.item_group,party=d.sales_partner_name,partner_type='Doctor',party_type='Sales Partner')				
+					# row.external_doctor_amount= flt(ex *self.exchange_rate)
+					row.total_external_doctor=flt(rate *row.qty)
+					if d.sales_partner_marketer:
+						row.marketer=d.sales_partner_marketer
+						rate=get_item_commission(item=d.item_code,item_group=row.item_group,partner_type='Marketer',party_type='Sales Partner')
+						# row.market_amount=flt(m*self.exchange_rate)
+						row.total_marketer=flt(rate*row.qty)
+
+			# frappe.msgprint("{0},{1}.{2}".format(row.internal_amount,row.external_doctor_amount,row.market_amount))
+			row.total_commission=(row.total_internal_doctor+row.total_external_doctor+row.total_marketer)*self.exchange_rate
 			row.remaining_amount=row.amount-row.total_commission
-			row.external_account=frappe.db.get_single_value("PAV Healthcare Settings", "external_doctor_account")
-			row.internal_account=frappe.db.get_single_value("PAV Healthcare Settings", "internal_doctor_account")
-			row.marketer_account=frappe.db.get_single_value("PAV Healthcare Settings", "marketer_account")
 			row.sii_no=d.sii_no
+		if not self.external_account: self.external_account=frappe.db.get_single_value("PAV Healthcare Settings", "external_doctor_account")
+		if not self.internal_account: self.internal_account=frappe.db.get_single_value("PAV Healthcare Settings", "internal_doctor_account")
+		if not self.marketer_account: self.marketer_account=frappe.db.get_single_value("PAV Healthcare Settings", "marketer_account")
+		
+		self.save()		
+		self.reload()
 
-def get_marketer(dr):
-	return frappe.db.sql("""select sales_partner_marketer from `tabSales Partner` where partner_type='Doctor'
-	and name=%s 
-	""",dr, as_dict=True)
+# def get_marketer(dr):
+# 	return frappe.db.sql("""select sales_partner_marketer from `tabSales Partner` where partner_type='Doctor'
+# 	and name=%s 
+# 	""",dr, as_dict=True)
 
-def get_item_commission(item=None,item_group=None, party=None,type=None):
+def get_item_commission(item=None,item_group=None, party=None,partner_type=None,party_type=None):
+	# it_doc = frappe.db.sql("""select rate from `tabItem Commission` where item=%s and party is null and partner_type is null and party_type is null and disabled=0 limit 1""",[item])	
+	it_doc=None
+	if not it_doc and item and partner_type:
+		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item' and item=%s and partner_type=%s and disabled=0 limit 1""",[item,partner_type])
+	if not it_doc and item_group and partner_type:
+		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item Group' and item=%s and partner_type=%s and disabled=0 limit 1""",[item_group,partner_type])
+	if not it_doc and item and party and party_type:
+		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item' and item=%s and party=%s and party_type=%s and disabled=0 limit 1""",[item,party,party_type])
+	if not it_doc and item_group and party and party_type:
+		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item Group' and item=%s and party=%s and party_type=%s and disabled=0 limit 1""",[item_group,party,party_type])
+	if not it_doc and item and party_type:
+		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item' and item=%s and party_type = %s and party is null and disabled=0 limit 1""",[item,party_type])
+	if not it_doc and item_group and party_type:
+		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item Group' and item=%s and party_type = %s and party is null and disabled=0 limit 1""",[item_group,party_type])
+	if not it_doc:
+		it_doc = frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item' and item=%s and party is null and partner_type is null and party_type is null and disabled=0 limit 1""",[item])	
+	if not it_doc:
+		it_doc = frappe.db.sql("""select rate from `tabItem Commission` where item_type='Item Group' and item=%s and party is null and partner_type is null and party_type is null and disabled=0 limit 1""",[item_group])	
+
 	
-	it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item=%s
-	and party=%s and disabled=0
-	""",[item,party], as_dict=True)
-	if not it_doc:
-		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item=%s
-		and party=%s and disabled=0
-	""",[item_group,party], as_dict=True)
-	if not it_doc:
-		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item=%s  and party is null and disabled=0
-	""",[item], as_dict=True)
-	if not it_doc:
-		it_doc= frappe.db.sql("""select rate from `tabItem Commission` where item=%s and party is null and disabled=0
-	""",[item_group], as_dict=True)
-
-	return it_doc
+	rate=0.0
+	if it_doc:
+		rate=it_doc[0][0]
+	# frappe.msgprint("rate={0}".format(rate))
+	return rate
 
 
 
 def get_item_details(args=None):
-	item = frappe.db.sql("""select i.name,i.item_group, id.income_account,id.expense_account, id.default_warehouse, id.cost_center, id.project, id.project_activities 
-			from `tabItem` i LEFT JOIN `tabItem Default` id ON i.name=id.parent and id.company=%s
-			where i.name=%s
-				and i.disabled=0
-				and (i.end_of_life is null or i.end_of_life='0000-00-00' or i.end_of_life > %s)""",
-			(args.get('company'), args.get('item_code'), nowdate()), as_dict = True)
+	item = frappe.db.sql("""select i.name,i.item_group
+			from `tabItem` i 
+			where i.name=%s """,(args.get('item_code')), as_dict = True)
 	if not item:
 			frappe.throw(_("Item {0} is not active or end of life has been reached").format(args.get("item_code")))
 
@@ -212,11 +252,9 @@ from erpnext.accounts.utils import get_company_default
 def get_item_detail(company,item_code):
 	item_dict = {}
 	item_details = get_item_details({'item_code': item_code, 'company': company})
-	item_dict['income_account'] = (item_details.get("income_account") or get_item_group_defaults(item_code, company).get("income_account") or 
-			get_company_default(company, "default_income_account") or frappe.get_cached_value('Company',  company, 
-			"default_income_account"))
+	# item_dict['income_account'] = (item_details.get("income_account") or get_item_group_defaults(item_code, company).get("income_account") or 
+			# get_company_default(company, "default_income_account") or frappe.get_cached_value('Company',  company, 
+			# "default_income_account"))
 	item_dict['item_group']=item_details.get('item_group')
-	item_dict['cost_center'] = item_details.get('cost_center')
-	item_dict['project'] = item_details.get('project')
-	item_dict['project_activities'] = item_details.get('project_activities')
+	# item_dict['cost_center'] = item_details.get('income_cost_center')
 	return item_dict
--- pav_healthcare/pav_healthcare/doctype/pav_healthcare_settings/pav_healthcare_settings.js
+++ pav_healthcare/pav_healthcare/doctype/pav_healthcare_settings/pav_healthcare_settings.js
@@ -2,7 +2,33 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('PAV Healthcare Settings', {
-	// refresh: function(frm) {
-
-	// }
+currency: function(frm) {
+	frm.set_query("external_doctor_account", function() {
+	return {
+		filters: {
+			"account_type": "Payable",
+			"company": frm.doc.company,
+			"account_currency":frm.doc.currency
+		}
+	}
+})
+frm.set_query("internal_doctor_account", function() {
+	return {
+		filters: {
+			"account_type": "Payable",
+			"company": frm.doc.company,
+			"account_currency":frm.doc.currency
+		}
+	}
+})
+frm.set_query("marketer_account", function() {
+	return {
+		filters: {
+			"account_type": "Payable",
+			"company": frm.doc.company,
+			"account_currency":frm.doc.currency
+		}
+	}
+})
+}
 });
